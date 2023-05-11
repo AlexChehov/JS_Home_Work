0e191723c5371675785662d7aae4fffc3dad1ce8
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-05-11 10:29:10 +0300

    11052023

---
+++ data.js
@@ -0,0 +1,29 @@
+let songs = [
+
+    {
+        name: 'Song1',
+        path:'trec1.mp3',
+        artist:'Jerry Heil',
+    },
+    {
+        name: 'Song2',
+        path:'trec2.mp3',
+        artist:'artist 2',
+    },
+    {
+        name: 'Song3',
+        path:'trec3.mp3',
+        artist:'artist 3',
+    },
+    {
+        name: 'Song4',
+        path:'trec4.mp3',
+        artist:'artist 4',
+    },
+    {
+        name: 'Song5',
+        path:'trec5.mp3',
+        artist:'artist 5',
+    },
+
+]
